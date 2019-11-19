756b2548a9bdc274b98ab5cbdbe7389181cd717f
WebXander <ruslanwebxander@gmail.com>
2019-11-19 12:23:59 +0300

    заполнение миграций + правка readme

--- database/migrations/2019_11_19_085505_create_users_cities_table.php
+++ database/migrations/2019_11_19_085505_create_users_cities_table.php
@@ -16,8 +16,11 @@ class CreateUsersCitiesTable extends Migration
         Schema::create('users_cities', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-
+            $table->integer('city_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('city_id')->references('id')->on('cities');
         });
     }
 
