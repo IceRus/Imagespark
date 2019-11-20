1d99b921573d2d5d0601d18c516854116f9d9b8e
WebXander <ruslanwebxander@gmail.com>
2019-11-20 12:38:34 +0300

    unique user_id

--- database/migrations/2019_11_19_085505_create_users_cities_table.php
+++ database/migrations/2019_11_19_085505_create_users_cities_table.php
@@ -14,8 +14,7 @@ class CreateUsersCitiesTable extends Migration
     public function up()
     {
         Schema::create('users_cities', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
+            $table->integer('user_id')->unsigned()->unique();
             $table->integer('city_id')->unsigned();
             $table->timestamps();
 
